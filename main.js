338a028f75ee0c46c28c485bbaaaff80f1401649
Antonella Cino <antonella.cino18@gmail.com>
2020-03-19 14:49:33 +0100

    analisi esercizio

---
+++ main.js
@@ -0,0 +1,25 @@
+/* calcolo biglietto del treno con relativi sconti diversi per le categorie che ci rientrano
+DESCRIZIONE:
+ll programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+Il prezzo del biglietto è definito in base ai km (0.21 € al km),
+ma va applicato uno sconto del 20% per i minorenni
+e del 40% per gli over 65.*/
+
+
+
+//creo la variabile dell'età del passeggero e la salvo in una variabile
+
+//creo la variabile dei km da percorrere e li salvo in una variabile
+
+//creo la variabile costo del biglietto
+
+//chiedo al passeggero la sua età
+
+//chiedo al passeggero i km che deve percorrere
+
+//moltiplico i km per il costo al km che e la salvo in una variabile
+
+//eseguo i controlli sull'eta del passeggero e verifico se c'è uno sconto
+
+//rimando a video i risultati
